cd4d6643ed25ec15170f12f4abded4267e83b45c
Ben <ben.sullivan1992@gmail.com>
2024-04-06 12:04:14 +0100

    Added basic angular layout

--- cardinal_toe_app/src/app/app.component.ts
+++ cardinal_toe_app/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, ContentComponent, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
