7fec39a8334ee9cae0296e81c2266cc1963ee6e4
Karl Minton <kminton@frontier.co.uk>
2024-04-10 14:20:46 +0100

    wrong turn

--- testbed/index.js
+++ testbed/index.js
@@ -61,45 +61,69 @@ class Generator
 
     previousNode = null;
 
+    DEVIATION_LIMIT = 3;
+
     constructor(nodeData)
     {
         this.nodeOptions = nodeData['nodes'];
     }
 
-    generate = () => {
+    /**
+     * @param {boolean} isCorrectPath a boolean for tracking if current path should terminate with a victory
+     * @param {Number} depth how many levels deep we are
+     * @param {Number} deviation increments only when not on successful path
+     * 
+     * @returns {void}
+     */
+    generate = (isCorrectPath = true, depth = 0, deviation = 0) => {
         if (this.previousNode === null) {
             this.rootNode = this.makeNode();
             this.previousNode = this.rootNode;
         }
 
-        // return the full tree - currently just returning void
+        if (deviation > this.DEVIATION_LIMIT) {
+            return;
+        }
+
         if (this.previousNode.isFinalNode === true) {
             return;
         }
 
-        // change 1 to random
-        for (let i = 0; i < 1; i++) {
+        let pathNumber = this.getNumberOfPaths();
+
+        // choose the route which is 'toward the objective' 
+        // All others must result in dead-ends after a small number of nodes
+        const successfulPathIndex = this.getRandomNumber(pathNumber) - 1;
+
+        for (let i = 0; i < pathNumber; i++) {
+            if (i !== successfulPathIndex) {
+                deviation++;
+                isCorrectPath = false;
+            }
+
+
+            
             let newNode = this.makeNode();
             this.previousNode.nodes.push(newNode);
             this.previousNode = newNode;
-            this.generate();
+            this.generate(isCorrectPath, depth, deviation);
         }
     }
 
+    /**
+     * 
+     * @returns {Node}
+     */
     makeNode = () => {
         const lastKey = this.nodeOptions.length - 1;
         const key = this.getRandomNumber(lastKey);
         const data = this.nodeOptions[key];
-
-        // console.log(key);
-        // console.log(this.nodeOptions);
 
         // Keep this as an empty array for now - possibly a job for recursion
         const nodes = [];
         // again worry about this when recurring
         const previousNode = null;
 
-        // ditto - added limit for testing
         let isFinalNode = false;
         if (this.iteration === 5) {
             isFinalNode = true;
@@ -110,8 +134,38 @@ class Generator
         return new Node(data.name, data.description, nodes, previousNode, isFinalNode)
     }
 
+    /**
+     * @description returns possible number of paths weighted towards 1
+     * 
+     * @returns {Number}
+     */
+    getNumberOfPaths = () => {
+        const randomInt = this.getRandomNumber(10);
+
+        console.log(`getNumberOfPaths randomInt - ${randomInt}`)
+
+        if (randomInt <= 6) {
+            return 1;
+        }
+
+        if (randomInt <= 9) {
+            return 2;
+        }
+
+        return 3;
+    }
+
+    /**
+     * @description returns a random number between 0 and 'maxNumber'
+     * 
+     * @param {Number} maxNumber 
+     * @param {Number} precision 
+     * @param {Number} multiplier 
+     * 
+     * @returns {Number}
+     */
     getRandomNumber = (maxNumber, precision = 1, multiplier = 10) => {
-        const randomInt = Math.round(
+        const randomInt = Math.ceil(
             Math.random().toPrecision(precision) * multiplier
         );
         
@@ -121,5 +175,4 @@ class Generator
 
 let gen = new Generator(nodeData);
 gen.generate();
-
 console.log(gen.rootNode);
